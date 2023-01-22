138fffce6253e2d70e1942dadf1f7e22828d1539
Santiago Varela Daza <svarela03@uan.edu.co>
2023-01-21 23:30:04 -0500

    #1 CORS

--- psiconit_project/settings.py
+++ psiconit_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'auth_app'
+    'auth_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'psiconit_project.urls'
@@ -154,3 +156,5 @@ SIMPLE_JWT = {
 }
 
 # ***********************< AUTH SETTINGS >***********************
+
+CORS_ALLOW_ALL_ORIGINS = True
